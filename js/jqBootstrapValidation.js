6f2db214f85a5b33bdef1a810834e88c43c536d1
Fachas Kévin <kfachas@omedo.co>
2022-05-05 09:50:00 +0200

    add new classes

--- js/jqBootstrapValidation.js
+++ js/jqBootstrapValidation.js
@@ -791,7 +791,7 @@
 				name: "Validemail",
 				type: "regex",
 				regex: "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\.[A-Za-z]{2,10}",
-				message: "Not a valid email address<!-- data-validator-validemail-message to override -->"
+				message: "Le format de l'email n'est pas valide. <!-- data-validator-validemail-message to override -->"
 			},
 			passwordagain: {
 				name: "Passwordagain",
